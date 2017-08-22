c1d7aae69f4383fb2cd27309aaa4b20bb7d98d22
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-08-22 10:48:36 +0200

    rwd

--- src/js/Classes/Slider.js
+++ src/js/Classes/Slider.js
@@ -61,34 +61,121 @@
 	
 		var nav = $('.js-popularNav'),
 			current_page = $('.js-popularNav .current'),
-			pages= $('.js-popularNav .all');
+			pages= $('.js-popularNav .all'),
+			reinit = false;
 
 
 		var carousel = document.querySelectorAll('.js-popular');
 		
-				
-		var owl = $(carousel).on('initialized.owl.carousel', function(e) {
-
-			pages.text(e.item.count);
-
-			}).owlCarousel({
-				animateOut: 'fadeOut',
-				autoplay: false,
-				items: 1,
-				lazyLoad: true,
-				loop: true,
-				nav: true,
-				navContainer: '.c-popular__nav',
-				navText: ''
-			});
-
-			owl.on('translate.owl.carousel', function(e) {
-				//page.text(e.page.index);
-				current_page.html(e.page.index +1);
-				
-			});
+		var carousel_settings;
+		
+		var carousel_settings_desktop = {
+			animateOut: 'fadeOut',
+			autoplay: false,
+			items: 1,
+			lazyLoad: true,
+			loop: true,
+			nav: true,
+			navContainer: '.c-popular__nav',
+			navText: ''
+		};
+		
+		var carousel_settings_mobile = {
+			autoplay: false,
+			lazyLoad: true,
+			loop: false,
+			nav: true,
+			navContainer: '.c-popular__nav',
+			navText: '',
+			responsive: {
+				0: {
+					items: 2
+				},
+				640: {
+					items: 3
+				},
+				840: {
+					items: 4
+				}
+			}
+		};
+		
+		function initialize() {
+		
+			if ($(window).width() > 1024) {
+				carousel_settings = carousel_settings_desktop;
+			} else {
+				carousel_settings = carousel_settings_mobile;
+			}
 			
+
+		
+			var owl = $(carousel).on('initialized.owl.carousel', function(e) {
+	
+				pages.text(e.item.count);
+	
+				}).owlCarousel(carousel_settings);
+	
+				owl.on('translate.owl.carousel', function(e) {
+					//page.text(e.page.index);
+					if ($(window).width() < 1025) {
+						current_page.html(e.item.index +1);
+					} else {
+						current_page.html(e.page.index +1);
+					}
+				});
+		}
+
+
+		if ($(window).width() < 1025) {
+			$('.c-popular__item').unwrap();
+			$('.row').remove();	
+			initialize();
+			reinit = true;
+		} else {
+			initialize();
+		}
 			
+		
+		$(window).on('resize', function() {
+			
+			if ($(window).width() < 1025) {
+				if (reinit === false) {
+				
+					$(carousel).trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+					$(carousel).find('.owl-stage-outer').children().unwrap();	
+					
+					
+					$('.c-popular__item').unwrap();
+					$('.row').remove();				
+					
+					$(carousel).addClass('owl-carousel');
+					initialize();
+					
+					reinit = true;
+					console.log('1');
+				}
+
+			} else {
+				if (reinit === true) {
+				
+					console.log('2');
+					$(carousel).trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+					$(carousel).find('.owl-stage-outer').children().unwrap();
+					
+					var divs = $(".c-popular__item");
+					for(var i = 0; i < divs.length; i+=5) {
+						divs.slice(i, i+5).wrapAll("<div class='row'></div>");
+					}
+					
+					$(carousel).addClass('owl-carousel');
+					initialize();
+					
+					reinit = false;
+				}
+			}
+			
+		});			
 	};
 
 
